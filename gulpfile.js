edb9e015a334593f10fa9887600cc256e7e88118
DLRandy <1208484996@qq.com>
2016-02-18 17:07:30 +0800

    'init'

---
+++ gulpfile.js
@@ -0,0 +1,9 @@
+var gulp = require('gulp');
+gulp.task('default', function () {
+	gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js',
+		'bower_components/jquery/dist/jquery.js'])
+	.pipe(gulp.dest('static/js'));
+	gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css',
+		'bower_components/bootstrap/dist/css/bootstrap-theme.min.css'])
+	.pipe(gulp.dest('static/css'));
+});
