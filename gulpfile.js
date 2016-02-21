c8b3e156e3f2189ec76d953985cb2aec8600eb77
DLRandy <1208484996@qq.com>
2016-02-21 10:07:13 +0800

    'dev-done'

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,12 @@
 var gulp = require('gulp');
 gulp.task('default', function () {
 	gulp.src(['bower_components/bootstrap/dist/js/bootstrap.min.js',
-		'bower_components/jquery/dist/jquery.js'])
+		'bower_components/jquery/dist/jquery.js',
+		'bower_components/bootstrap-treeview/dist/bootstrap-treeview.min.js'])
 	.pipe(gulp.dest('static/js'));
 	gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css',
-		'bower_components/bootstrap/dist/css/bootstrap-theme.min.css'])
+		'bower_components/bootstrap/dist/css/bootstrap-theme.min.css',
+		'bower_components/bootstrap-treeview/dist/bootstrap-treeview.min.css'])
 	.pipe(gulp.dest('static/css'));
 	gulp.src('bower_components/bootstrap/dist/fonts/*')
 	.pipe(gulp.dest('static/fonts'));
